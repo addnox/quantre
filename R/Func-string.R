8ea679040e9c87dadcad95a5fb2ec8cac4839e97
addnox <addnox@gmail.com>
2024-08-16 13:24:19 +0800

    add num and num_format

--- R/Func-string.R
+++ R/Func-string.R
@@ -156,3 +156,51 @@ stri_wrap0 <- function (string, width = 80, indent = 0, exdent = 0, whitespace_o
   vapply(out, paste, character(1), collapse = "\n")
 }
 
+#' Format numbers
+#'
+#' @export
+#' @examples
+#' x <- c(1.23e12, 3.45e7, -6.78e3, Inf, NA)
+#' num(x)
+#' num(x, "M", digits = 1)
+#' num(x, "B", inf.label = "Unlimited", na.label = "")
+num <- function(x, scale = c("auto", "C", "K", "M", "B", "P", "pp"), digits = 2, na.label = "N/A", inf.label = "Inf", ...) {
+  scale <- match.arg(scale)
+  .accuracy <- 1 / 10 ^ digits
+
+  if (scale == "auto") {
+    tmp_scale <- cut(abs(x), c(0, 1e3, 1e6, 1e9, Inf), c("C", "K", "M", "B"))
+    final_scale_label <- names(which.min(table(tmp_scale)))
+    if (is.na(final_scale_label)) final_scale_label <- "C"
+    final_scale_factor <- switch(final_scale_label, "C" = 1, "K" = 1e3, "M" = 1e6, "B" = 1e9)
+    if (final_scale_label == "C") final_scale_label <- ""
+
+    res <- comma(x, digits, final_scale_factor, final_scale_label)
+
+  } else {
+    final_scale_factor <- switch(scale, "C" = 1, "K" = 1e3, "M" = 1e6, "B" = 1e9, "P" = .01, "pp" = .01)
+    final_scale_label <- switch(scale, "C" = "", "K" = "K", "M" = "M", "B" = "B", "P" = "%", "pp" = " p.p.")
+
+    res <- comma(x, digits, final_scale_factor, final_scale_label)
+  }
+
+  res <- ifelse(is.na(x), na.label, res)
+  res <- ifelse(is.infinite(x), inf.label, res)
+
+  return(res)
+}
+
+
+#' @export
+#'@rdname num
+num_format <- function(scale = c("auto", "C", "K", "M", "B", "P", "pp"), digits = 0, ...) {
+  scale <- match.arg(scale)
+  f <- function(x) num(x, scale, digits, ...)
+  f
+}
+
+comma <- function(x, digits = 2, scale_factor = 1, scale_label = "") {
+  x1 <- round(x / scale_factor, digits = digits)
+  c1 <- format(x1, nsmall = digits, big.mark = ",", big.interval = 3, scientific = FALSE, trim = TRUE)
+  paste0(c1, scale_label)
+}
